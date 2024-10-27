dfdbc4efba9c53d1e1746c2664e159f90e76a853
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-10-27 18:17:12 -0500

    Completed globom43

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c45b0dcac01824facc97dfcb3f3a19aaee5da7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c625f7479230b1480ec18b6675bea0f73fedca58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
